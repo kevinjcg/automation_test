c6fb839133f6eec1f54c6b25029322936a7724ff
kevinjcg <kevinj.calerogmail.com>
2022-01-30 21:11:47 -0800

    Comment unit tests for login

--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -3,24 +3,24 @@ import LoginPage from  '../pageobjects/login.page';
 let username = 'standard_user';
 let password = 'secret_sauce';
 
-describe('Verify Login', () => {
+// describe('Verify Login', () => {
 
-    it('Verify input are empty', () => {
-        LoginPage.open('');
+//     it('Verify input are empty', () => {
+//         LoginPage.open('');
         
-        expect(LoginPage.inputUsername.getText()).toBe('');
-        expect(LoginPage.inputPassword.getText()).toBe('');
-    });
+//         expect(LoginPage.inputUsername.getText()).toBe('');
+//         expect(LoginPage.inputPassword.getText()).toBe('');
+//     });
 
-    it('Verify that the input contains their respective values', () => {
-        LoginPage.inputUsername.setValue(username);
-        LoginPage.inputPassword.setValue(password);
-        expect(LoginPage.inputUsername).toHaveValue(username);
-        expect(LoginPage.inputPassword).toHaveValue(password);
-    });
+//     it('Verify that the input contains their respective values', () => {
+//         LoginPage.inputUsername.setValue(username);
+//         LoginPage.inputPassword.setValue(password);
+//         expect(LoginPage.inputUsername).toHaveValue(username);
+//         expect(LoginPage.inputPassword).toHaveValue(password);
+//     });
 
-    it('Verify user was logged in successfully', () => {
-        LoginPage.login(username, password);
-        expect(browser).toHaveUrlContaining('/inventory.html');
-    });
-});
+//     it('Verify user was logged in successfully', () => {
+//         LoginPage.login(username, password);
+//         expect(browser).toHaveUrlContaining('/inventory.html');
+//     });
+// });
